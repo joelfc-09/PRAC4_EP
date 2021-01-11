de05fd76ddb3c64845dd34cd4ddc88f5bc3b589a
gabygarcia98 <gabygarcia211998@gmail.com>
2021-01-11 19:26:12 +0100

    productID i productIDTest finished.
    Complete my task for today :D

--- src/data/ProductID.java
+++ src/data/ProductID.java
@@ -1,5 +1,31 @@
 package data;
 
 final public class ProductID {
-     //TODO
+
+    private final String productID;
+
+    public ProductID(String code) {
+        this.productID = code;
+    }
+
+    public String getProductID() {
+        return productID;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProductID finalProductID = (ProductID) o;
+        return productID.equals(finalProductID.productID);
+    }
+
+    @Override
+    public int hashCode() { return productID.hashCode(); }
+
+    @Override
+    public String toString() {
+        return "ProductID{" + "personal code='" + productID + '\'' + '}';
+    }
 }
+
